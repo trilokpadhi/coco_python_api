c35e5865879179ceee0a2eac730f6afa30dda451
Trilok Padhi <trilokpadhi07@gmail.com>
2021-04-07 15:48:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
     )
 ]
 
